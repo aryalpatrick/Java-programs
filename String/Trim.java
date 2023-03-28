9403da56c4b981d3b23788ec5ce6bb1d2012b0d1
Patrick <83208684+aryalpatrick@users.noreply.github.com>
2023-03-28 11:08:37 +0545

    Create Trim.java

---
+++ String/Trim.java
@@ -0,0 +1,9 @@
+//trim method trims the blankspace in a string
+public class Trim {
+
+    public static void main(String[] args) {
+        String m ="   hi";
+
+        System.out.println(m.trim());
+    }
+}
