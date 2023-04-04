17f36b98b192ffbaa4a2bf452ed4bf583472dc72
Pratik Aryal <83208684+aryalpatrick@users.noreply.github.com>
2023-04-04 10:41:51 +0545

    Create temperature.java

---
+++ Fahrenheit to Celsius/temperature.java
@@ -0,0 +1,17 @@
+//temperature
+import java.util.Scanner;
+
+public class temperature {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();
+
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+
+        System.out.printf("In celcius: "+celsius);
+    }
+}
